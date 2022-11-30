922cc6735ff070def0d940e2d383a8461301fd4e
kimyihyung <kimlh3743@gmail.com>
2022-11-30 11:31:37 +0900

    .

--- connect/connect.php
+++ connect/connect.php
@@ -1,8 +1,8 @@
 <?php
     $host = "localhost";
-    $user = "vvv0032";
-    $pass = "Hhd3712493!";
-    $db = "vvv0032";
+    $user = "k5249";
+    $pw = "kimklh1009!%";
+    $db = "k5249";
 
     $connect = new mysqli($host, $user, $pass, $db);    
     $connect -> set_charset("utf8");
