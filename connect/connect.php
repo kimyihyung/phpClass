141b8241891fb6537a61dae842a2f5c962185970
kimyihyung <kimlh3743@gmail.com>
2022-11-30 11:56:49 +0900

    .

--- connect/connect.php
+++ connect/connect.php
@@ -3,7 +3,11 @@
     $user = "k5249";
     $pw = "kimklh1009!%";
     $db = "k5249";
-
-    $connect = new mysqli($host, $user, $pass, $db);    
+    $connect = new mysqli($host, $user, $pw, $db);    
     $connect -> set_charset("utf8");
+    if(mysqli_connect_errno()){
+        echo "database connect false";
+    } else {
+        //echo "database connect true";
+    }
 ?>
